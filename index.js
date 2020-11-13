8fc71084cf90054de1b3bd1fffbcd5fe8ba49f4e
luck21honey <lukeadamdev@gmail.com>
2020-11-13 04:32:48 +0000

    update sendEncryptedMail module

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var mail = require('./sendEncryptedMail');
+var param = {
+    from: "pandamoney425@gmail.com",
+    to: "peacehoney425@gmail.com",
+    subject: "This email is encrypted",
+    body: "This is a digitally encrypted mail",
+    attachPath: __dirname + "/../source/test.csv",
+    certPath: __dirname + "/../source/ELD_RSA.crt"
+}
 
-sendEncryptedMail();
+mail.sendEncryptedMail(param);
--- sendEncryptedMail.js
+++ sendEncryptedMail.js
@@ -19,62 +19,63 @@ if (os.platform() == 'win32') {
     var chilkat = require('@chilkat/ck-node12-macosx');
 }
 
-function sendEncryptedMail() {
-    // Replace license key after purchased
-    var glob = new chilkat.Global();
-    var success = glob.UnlockBundle("Anything for 30-day trial");
-    if (success !== true) {
-        console.log("Unlock error>>>", glob.LastErrorText);
-        return;
+module.exports = {
+    async sendEncryptedMail(param) {
+        // Replace license key after purchased
+        var glob = new chilkat.Global();
+        var success = glob.UnlockBundle("Anything for 30-day trial");
+        if (success !== true) {
+            console.log("Unlock error>>>", glob.LastErrorText);
+            return;
+        }
+    
+        var status = glob.UnlockStatus;
+        if (status == 2) {
+            console.log("Unlocked using purchased unlock code.");
+        }
+        else {
+            console.log("Unlocked in trial mode.");
+        }
+        // console.log("Trial more, or with a purchased unlock code? >>> ", glob.LastErrorText);
+    
+    
+        // The mailman object is used for sending and receiving email.
+        var mailman = new chilkat.MailMan();
+    
+        // Set the SMTP server
+        mailman.SmtpHost = process.env.SMTP_HOST;
+        mailman.SmtpUsername = process.env.SMTP_USERNAME;
+        mailman.SmtpPassword = process.env.SMTP_PASSWORD;
+        mailman.SmtpSsl = process.env.SMTP_SSL;
+        mailman.SmtpPort = process.env.SMTP_PORT;
+
+        // Load the .cer file into a certificate object.
+        var cert = new chilkat.Cert();
+        var success = cert.LoadFromFile(param.certPath);
+        if (success !== true) {
+            console.log(cert.LastErrorText);
+            return;
+        }
+        console.log('Load Cert Success!!!', success);
+    
+        var email = new chilkat.Email();
+    
+        email.Subject = param.subject;
+        email.Body = param.body;
+        email.From = param.from;
+        success = email.AddTo(param.to, param.to); // fmcsaeldsub@dot.gov
+    
+        // Indicate that the email is to be sent encrypted.
+        email.SendEncrypted = true;
+        // Specify the certificate to be used for encryption.
+        success = email.SetEncryptCert(cert);
+    
+        success = mailman.SendEmail(email);
+        if (success !== true) {
+            console.log('Error in sending email>>>', mailman.LastErrorText);
+        }
+        else {
+            console.log("Mail Sent!");
+        }
     }
-
-    var status = glob.UnlockStatus;
-    if (status == 2) {
-        console.log("Unlocked using purchased unlock code.");
-    }
-    else {
-        console.log("Unlocked in trial mode.");
-    }
-    console.log("trial more, or with a purchased unlock code? >>> ", glob.LastErrorText);
-
-
-    // The mailman object is used for sending and receiving email.
-    var mailman = new chilkat.MailMan();
-
-    // Set the SMTP server
-    mailman.SmtpHost = "smtp.gmail.com";
-    mailman.SmtpUsername = "";
-    mailman.SmtpPassword = "";
-    mailman.SmtpSsl = true;
-    mailman.SmtpPort = 465;
-
-    // Load the .cer file into a certificate object.
-    var cert = new chilkat.Cert();
-    var success = cert.LoadFromFile("../source/ELD_RSA.crt");
-    if (success !== true) {
-        console.log(cert.LastErrorText);
-        return;
-    }
-    console.log('Load Cert Success!!!', success);
-
-    var email = new chilkat.Email();
-
-    email.Subject = "This email is encrypted";
-    email.Body = "This is a digitally encrypted mail";
-    email.From = "";
-    success = email.AddTo("Chilkat Blog", ""); // fmcsaeldsub@dot.gov
-
-    // Indicate that the email is to be sent encrypted.
-    email.SendEncrypted = true;
-    // Specify the certificate to be used for encryption.
-    success = email.SetEncryptCert(cert);
-
-    success = mailman.SendEmail(email);
-    if (success !== true) {
-        console.log('Error in sending email>>>', mailman.LastErrorText);
-    }
-    else {
-        console.log("Mail Sent!");
-    }
-
 }
