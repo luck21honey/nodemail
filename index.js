d7226eb1783a3a521a763034c44e25bbccdbc07a
luck21honey <lukeadamdev@gmail.com>
2020-11-13 17:50:37 +0000

    update cerpath

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var param = {
     body: "This is a digitally encrypted mail",
     attachPath:  __dirname + "/../source/test.csv",
     pfxPath: __dirname + "/../source/ELD_RSA.pfx",
-    cerPath:  __dirname + "/../source/ELD_RSA.crt"
+    cerPath:  __dirname + "/../source/FMCSA_ELD_Expires_April2023_Certificate.cer"
 }
 
 mail.sendEncryptedMail(param);
